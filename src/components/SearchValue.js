21bd1b9e8dff9cfd4e3ed39bea094ecf7e0a6cc6
Dhaval <dhavaldodiya96@gmail.com>
2024-03-17 11:39:27 +0530

    Code clean up

---
+++ src/components/SearchValue.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchValue = ({ fiterData }) => {
+  console.log(fiterData, "kkkkk");
+  return (
+    <div>
+      <button>SearchValue</button>
+    </div>
+  );
+};
+
+export default SearchValue;
